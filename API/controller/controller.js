d20348c9fc428f5caa77c2921e899a5d2fbfe76e
Kaveen LK <kmklaksitha@gmail.com>
2021-10-02 18:37:04 +0530

    api structure created

--- API/controller/controller.js
+++ API/controller/controller.js
@@ -3,7 +3,10 @@ const Cart = require("../model/cart")
 
 //to add items to cart{
 
-router.post("addToCart", async (req, res) => {
+router.post("/addToCart", async (req, res) => {
+
+    console.log("data for cart", req.body)
+
     const name = req.body.name;
     const quantity = req.body.num;
     const price = req.body.price;
@@ -14,5 +17,29 @@ router.post("addToCart", async (req, res) => {
 
     try {
 
+        let response = await newCart.save();
+        if (response) {
+            return res.status(201).send({ status: "item added for cart" })
+        } else {
+            return res.status(500).send({ status: "failed to add item record" })
+        }
+
+    } catch (err) {
+        console.log("error", err)
     }
 })
+
+//to retrieve data from db
+router.get("/cart", async (req, res) => {
+
+    try {
+        const response = await Cart.find();
+        return res.status(200).send({ status: "Success", data: response });
+    } catch (error) {
+        console.log("Something went wrong while connecting to DB");
+        return { ok: false };
+    }
+
+});
+
+module.exports = router;
